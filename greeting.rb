3b856e1fb2457ab35c96a4d209f7744827247b50
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-03-08 20:59:21 +0000

    rubyを学ぼう

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+def greeting(name)
+  return "Hello,#{name}!"
+  "Good mormning, #{name}!"
+end
+
+puts greeting('John')
---
+++ lesson5.rb
@@ -0,0 +1,8 @@
+total_price = "所持金"
+if total_peice > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+else
+  puts "みかんを購入することができません。"
+end
